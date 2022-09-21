afd3687158e33625293d2b4460e66d3b3ecc0890
Jan Koupil <jan.koupil@delta-skola.cz>
2022-09-21 12:09:40 +0200

    2022-21-09 Cisla

---
+++ 01_Uvod_04_Promenne/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01_Uvod_04_Promenne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jmeno = "Karel"; //deklaruju proměnnou, alokuje se paměť, ukládám do ní hodnotu
+            Console.WriteLine(jmeno);
+
+            jmeno = "Františka";//měním hodnotu
+            Console.WriteLine(jmeno);
+
+            //string jmeno = "Alois";//nelze - klíčové slovo string znamená "udělej novou" ale už existuje
+
+            string mesto; //deklaruju proměnnou ale neukládám
+            //Console.WriteLine(mesto); //nelze - nepřiřazená proměnná
+
+        }
+    }
+}
---
+++ 01_Uvod_05_Cisla/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01_Uvod_05_Cisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int delenec = 19; //celočíselná proměnná
+            int delitel = 3;
+            int podil = delenec / delitel;
+
+            //Console.WriteLine(podil);
+            //Console.WriteLine("Podíl čísel " + delenec + " a " + delitel + " je " + podil + "."); //skládání řetězců
+            Console.WriteLine("Podíl čísel {0} a {1} je {2} ({0}/{1} = {2}).", delenec, delitel, podil);
+
+
+        }
+    }
+}

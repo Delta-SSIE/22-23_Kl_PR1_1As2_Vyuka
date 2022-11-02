d9036988772f3f95354c31fff7d1eaf24f514a95
Jan Koupil <jan.koupil@gmail.com>
2022-11-02 18:06:01 +0100

    2022-11-02 Další while

---
+++ 03_While_05_Nacitani_slova/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _03_While_05_Nacitani_slova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Zadej slovo, ukonči mezerníkem.");
+            //string slovo = "";
+            //int delka = 0;
+            //char znak = 'a';
+
+            //while (znak != ' ')
+            //{
+            //    znak = Console.ReadKey().KeyChar;
+            //    if (znak != ' ')
+            //    { 
+            //        slovo += znak;
+            //        delka++;
+            //    }
+            //}
+
+            //Console.WriteLine($"Zadané slovo bylo \"{slovo}\", jeho délka je {delka}.");
+
+            Console.WriteLine("Zadej slovo, ukonči mezerníkem.");
+            string slovo = "";
+            int delka = 0;
+            
+            char znak = Console.ReadKey().KeyChar;
+
+            while (znak != ' ')
+            {
+                slovo += znak;
+                delka++;
+                znak = Console.ReadKey().KeyChar;
+            }
+
+            Console.WriteLine($"Zadané slovo bylo \"{slovo}\", jeho délka je {delka}.");
+        }
+    }
+}

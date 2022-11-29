56efe2eb1406d0220758e5bfacd4ba26da41394e
Jan Koupil <jan.koupil@delta-skola.cz>
2022-11-29 13:00:25 +0100

    2022-11-29 Vnorene for-cykly

---
+++ 04_For_03_Algebrogram/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _04_For_03_Algebrogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int S = 1; S < 10; S++)
+            {
+                for (int E = 0; E < 10; E++)
+                {
+                    if (E == S) //přeskočím duplicitní hodnoty
+                        continue;
+
+                    for (int N = 0; N < 10; N++)
+                    {
+                        if (N == E || N == S)
+                            continue;
+
+                        for (int D = 0; D < 10; D++)
+                        {
+                            if (D == E || D == S || D == N)
+                                continue;
+
+                            for (int M = 1; M < 10; M++)
+                            {
+                                if (M == E || M == S || M == N || M == D)
+                                    continue;
+                                
+                                for (int O = 0; O < 10; O++)
+                                {
+                                    if (O == E || O == S || O == N || O == D || O == M)
+                                        continue;
+
+                                    for (int R = 0; R < 10; R++)
+                                    {
+                                        if (R == E || R == S || R == N || R == D || R == M || R == O)
+                                            continue;
+
+                                        for (int Y = 0; Y < 10; Y++)
+                                        {
+                                            if (Y == E || Y == S || Y == N || Y == D || Y == M || Y == O || Y == R)
+                                                continue;
+
+                                            int send = S * 1_000 + E * 100 + N * 10 + D;
+                                            int more = M * 1_000 + O * 100 + R * 10 + E;
+                                            int money = M * 10_000 + O * 1_000 + N * 100 + E * 10 + Y;
+                                            if (send + more == money)
+                                            {
+                                                Console.WriteLine($" {S}{E}{N}{D}");
+                                                Console.WriteLine($"+{M}{O}{R}{E}");
+                                                Console.WriteLine($"-----");
+                                                Console.WriteLine($"{M}{O}{N}{E}{Y}");
+                                                
+                                                Console.WriteLine();
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 04_For_04_Ctverec/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace _04_For_04_Ctverec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //char znak = '#';
+            //int strana = 4;
+
+            //for (int y = 0; y < strana; y++)
+            //{
+            //    for (int x = 0; x < strana; x++)
+            //    {
+            //        Console.Write(znak);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            ////obdelnik
+            //char znak = '#';
+            //int stranaA = 6;
+            //int stranaB = 4;
+
+
+            //for (int y = 0; y < stranaA; y++)
+            //{
+            //    for (int x = 0; x < stranaB; x++)
+            //    {
+            //        Console.Write(znak);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            // trojuhelnik b
+            //char znak = '#';
+            //int vyska = 6;
+
+
+            //for (int y = 0; y < vyska; y++)
+            //{
+            //    for (int x = 0; x < y + 1; x++)
+            //    {
+            //        Console.Write(znak);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //trojuhelnik p
+            //char znak = '#';
+            //int vyska = 6;
+
+
+            //for (int y = 0; y < vyska; y++)
+            //{
+            //    for (int x = 0; x < vyska - y; x++)
+            //    {
+            //        Console.Write(znak);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //trojuhelnik d
+            //char znak = '#';
+            //int vyska = 6;
+
+
+            //for (int y = 0; y < vyska; y++)
+            //{
+            //    int pocetMezer = vyska - y - 1;
+            //    int pocetZnaku = y + 1;
+
+            //    for (int x = 0; x < pocetMezer; x++)
+            //    {
+            //        Console.Write(' ');
+            //    }
+
+            //    for (int x = 0; x < pocetZnaku; x++)
+            //    {
+            //        Console.Write(znak);
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            //trojuhelnik q
+            //    char znak = '#';
+            //    int vyska = 6;
+
+
+            //    for (int y = 0; y < vyska; y++)
+            //    {
+            //        int pocetMezer = y;
+            //        int pocetZnaku = vyska - y;
+
+            //        for (int x = 0; x < pocetMezer; x++)
+            //        {
+            //            Console.Write(' ');
+            //        }
+
+            //        for (int x = 0; x < pocetZnaku; x++)
+            //        {
+            //            Console.Write(znak);
+            //        }
+
+            //        Console.WriteLine();
+            //    }
+
+
+            //trojuhelnik delta
+            char znak = '#';
+            int vyska = 6;
+
+
+            for (int y = 0; y < vyska; y++)
+            {
+                int pocetMezer = vyska - y - 1;
+                int pocetZnaku =  2 * y + 1;
+
+                for (int x = 0; x < pocetMezer; x++)
+                {
+                    Console.Write(' ');
+                }
+
+                for (int x = 0; x < pocetZnaku; x++)
+                {
+                    Console.Write(znak);
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+    
+}

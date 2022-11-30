63a533614a7dd6de654b0185ea19c2923d1c8031
Jan Koupil <jan.koupil@gmail.com>
2022-11-30 17:33:47 +0100

    2022-30-11 Caesar

---
+++ 04_For_05_Caesar/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _04_For_05_Caesar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string otevrenyText = "UTOK ZACNE VE TRI RANO U SEVERNI BRANY";
+            int klic = 3;
+
+            string sifrovyText = "";
+
+            //pro kazdy znak
+            for (int i = 0; i < otevrenyText.Length; i++)
+            {
+                char znak = otevrenyText[i];
+
+                if (znak >= 'A' && znak <= 'Z')
+                {
+                    //znak na cislo
+                    int cisloZnaku = znak;
+
+                    //prictu klic
+                    cisloZnaku += klic;
+
+                    if (cisloZnaku > 'Z')
+                        cisloZnaku -= 26;
+
+                    //prevod zpět na znak
+                    char sifrovyZnak = (char)cisloZnaku;
+
+                    //pridat k sifrovemu textu
+                    sifrovyText += sifrovyZnak;
+                }
+                else
+                {
+                    sifrovyText += znak;
+                }
+            }
+
+            // vypsat sifrovy text
+            Console.WriteLine(sifrovyText);
+        }
+    }
+}

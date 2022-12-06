8940f0a8abbce5c727272024eba0b1839a739c66
Jan Koupil <jan.koupil@delta-skola.cz>
2022-12-06 10:51:14 +0100

    2022-12-06 Sachove forcykly

---
+++ 04_For_06_Kam_vez_1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace _04_For_06_Kam_vez_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string vstup;
+            bool jeChyba;
+            char radek = '\0';
+            char sloupec = '\0';
+
+            do
+            {
+                jeChyba = false;
+
+                //načíst vstup
+                Console.WriteLine("Zadej pozici věže: ");
+                vstup = Console.ReadLine().ToUpper();
+
+                //zkontrolovat a rozdělit na souřadnice
+                if (vstup.Length != 2)
+                {
+                    Console.WriteLine("Neplatný vstup");
+                    jeChyba = true;
+                    continue;
+                }
+
+                radek = vstup[1];
+                sloupec = vstup[0];
+
+                if (radek < '1' || radek > '8' || sloupec < 'A' || sloupec > 'H')
+                {
+                    Console.WriteLine("Neplatný vstup");
+                    jeChyba = true;
+                    continue;
+                }
+            } 
+            while (jeChyba);
+
+            Console.WriteLine();
+            Console.WriteLine("Věž útočí na:");
+
+            //zachovat řádek, přidat všechny sloupce
+            for (int i = 0; i < 8; i++)
+            {
+                char cilSloupec = (char)('A' + i);
+                if (cilSloupec == sloupec)
+                    continue;
+
+                Console.WriteLine("" + cilSloupec + radek);
+            }
+
+            //zachovat sloupec, přidat všechny řádky
+            for (int i = 0; i < 8; i++)
+            {
+                char cilRadek = (char)('1' + i);
+                if (cilRadek == radek)
+                    continue;
+
+                Console.WriteLine("" + sloupec + cilRadek);
+            }
+        }
+    }
+}
---
+++ 04_For_07_Kam_vez_2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04_For_07_Kam_vez_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor white = ConsoleColor.DarkYellow;
+            ConsoleColor black = ConsoleColor.Black;
+            ConsoleColor text = ConsoleColor.DarkGray;
+
+            Console.ForegroundColor = text;
+
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    if ((x + y) % 2 != 0)
+                    {
+                        Console.BackgroundColor = black;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = white;
+                    }
+                    Console.Write("  ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
